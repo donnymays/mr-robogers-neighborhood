1fcead87c2e154ffb72058bffdc2ab8c18c62b5f
donnymays <dtmays2@gmail.com>
2020-08-21 09:32:54 -0700

    test passes with 2s and 3s for all numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,17 @@
 let robotArr = function(input) {
   let numArr = [];
   for (let i = 0; i <= input; i++) { 
-    if (i.toString().includes("1")) {
-       numArr.push("Beep!");
+    if (i.toString().includes("3")) {
+      numArr.push("Won't you be my neighbor?");
+    } else if (i.toString().includes("2")) {
+      numArr.push("Boop!");
+    } else if (i.toString().includes("1")) {
+      numArr.push("Beep!");
+      
     } else { 
       numArr.push(i);
     }
   }
   console.log(numArr);
 }
+
