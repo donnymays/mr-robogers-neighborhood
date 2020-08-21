4b0346203ddb8648e3a1c8773cecca1e60bff4fd
donnymays <dtmays2@gmail.com>
2020-08-21 08:40:40 -0700

    continue work on initial for loop

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Business Logic
+
+let robotArr = function(input) {
+  let numArr = [];
+  for (let i = 1; i <= input; i++) { 
+    if (i === "1") {
+       numArr.push("Beep!");
+    } 
+  }
+  console.log(numArr);
+}
