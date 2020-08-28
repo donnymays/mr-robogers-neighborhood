a07b67541b3409071e306adf373ccb339b2dfb28
donnymays <dtmays2@gmail.com>
2020-08-28 07:25:44 -0700

    seperated business and ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,43 @@
+var robotTranslate = function(numInput, nameInput) {
+  let numArr = [];
+   for (let i = 0; i <= numInput; i++) { 
+     if (i.toString().includes("3")) {
+       numArr.push("Won't you be my neighbor, " + nameInput + "?");
+     } else if (i.toString().includes("2")) {
+       numArr.push(" Boop!");
+     } else if (i.toString().includes("1")) {
+       numArr.push(" Beep!");   
+     } else { 
+       numArr.push(" " + i);
+     };
+   };
+   return numArr;
+ };  
+ 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();   
-var robotTranslate = function(numInput) {
-  for (let i = 0; i <= numInput; i++) { 
-    if (i.toString().includes("3")) {
-      numArr.push("Won't you be my neighbor, " + nameInput + "?");
-    } else if (i.toString().includes("2")) {
-      numArr.push(" Boop!");
-    } else if (i.toString().includes("1")) {
-      numArr.push(" Beep!");   
-    } else { 
-      numArr.push(" " + i);
-    };
-  };
-  return numArr;
-};  
 
     var numInput = parseInt($("input#numInput").val());
     var nameInput = $("input#name").val();
-   $("input#numInput").val("");
-   $("input#name").val("");
-   
+    $("input#numInput").val("");
+    $("input#name").val("");
     $('.name').text(nameInput);
 
-    numArr = [];
-    
     if (nameInput === "")  {
       alert("Please Enter Your Name!");
     } else {
-      var resultArray = [];
-      var resultArray = robotTranslate(numInput);
+      var resultArray = robotTranslate(numInput, nameInput);
+      
       resultArray.map(function(robotNum) {
         var resultList = $("#resultList");
         resultList.append("<span>" + robotNum + "</span>");
       });
-      $("span").each(function(index) {
-        $(this).delay(600*index).fadeIn(600) 
-      });
-
-    };    
+      $("resultlist#span").each(function(index) {
+        $(this).delay(600*index).fadeIn(600, function() {
+         $(this).delay(500).remove();
+        });
+      }); 
+    };  
   });
 });
